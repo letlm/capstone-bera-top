25b78b0514e5902305906252b0d4e7e839a59ec9
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-05-11 18:48:32 -0300

    fix: Alteração de páginas do Provider para melhorar a performance

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./providers/auth.jsx";
+import Providers from "./providers";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <BrowserRouter>
+      <Providers>
+        <App />
+      </Providers>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
