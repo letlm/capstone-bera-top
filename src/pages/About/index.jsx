2e1e7ceb23dfab624fff8b046d9d8a6074c3ef1d
Jhon Muller <jmuller.jhon80@gmail.com>
2022-05-12 17:32:20 -0400

    create: componente InfoDevs que recebe um objeto com informações sobre o dev para montar sua imagem na cadeira de bar e suas informações. Na página about foi criado um array de objeto para guardar as informações dos devs desenvolvedores.

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,36 +1,68 @@
 import { TitlePage, ParagraphOne, ParagraphTwo, Container } from "./styles";
+import { WhoDoes } from "./styles"
 import BeerImage from "../../assets/BeerImage79x74.png";
 import Feedback from "../../assets/necessidade46x55.png";
 import Proposito from "../../assets/proposito46x55.png";
-import { useAuth } from "../../providers/AuthProvider";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useHistory } from "react-router-dom";
 import Header from "../../components/Header";
+import InfoDevs from "../../components/InfoDevs";
+import Leo from "../../assets/leo.png"
+import Jhon from "../../assets/jhon.png"
+import Ana from "../../assets/ana.png"
+import Leticia from "../../assets/let.png"
+import Regis from "../../assets/regis.png"
+import Luan from "../../assets/luan.png"
 
 function About() {
-  const history = useHistory();
-  const { authenticated } = useAuth();
-  // const authenticated = true
-  const [textButton, setTextButton] = useState("Login");
+  
+  const devs = [
+    {
+      name: "Leonardo R.",
+      work: "Quality Assurance",
+      linkendin: "/leorocha",
+      github: "/leorocha",
+      image: Leo,
+    },
+    {
+      name: "Jhon Muller",
+      work: "Quality Assurance",
+      linkendin: "/jhonmuller",
+      github: "/jhonmuller",
+      image: Jhon,
+    },
+    {
+      name: "Ana Laura",
+      work: "Product Owner",
+      linkendin: "/analaura",
+      github: "/analaura",
+      image: Ana,
+    },
+    {
+      name: "Leticia L.",
+      work: "Tech Leader",
+      linkendin: "/leticia",
+      github: "/leticia",
+      image: Leticia,
+    },
+    {
+      name: "Régis S.",
+      work: "Scrum Master",
+      linkendin: "/regis",
+      github: "/regis",
+      image: Regis,
+    },
+    {
+      name: "Luan C.",
+      work: "Quality Assurance",
+      linkendin: "/luan",
+      github: "/luan",
+      image: Luan,
+    },
+  ];
 
-  useEffect(() => {
-    if (authenticated) {
-      setTextButton("Logout");
-    } else {
-      setTextButton("Login");
-    }
-  }, [authenticated]);
+  
 
   return (
     <Container>
-      {/* <Header>
-                <div className="logo" onClick={()=> history.push("/")} >
-                    <h1>BeraTop</h1>
-                    <img src={Logo} alt="Logo da BeraTop"></img>
-                </div>
-                <button onClick={ ()=> history.push("/login") } >{textButton}</button>
-            </Header> */}
       <Header bgColor={"#c4c4c4"}></Header>
       <div>
         <div>
@@ -55,6 +87,32 @@ function About() {
               aquela cerveja que tanto pensou em experimentar.
             </p>
           </ParagraphTwo>
+        </div>
+
+        <div>
+          <WhoDoes>
+            <p>Quem faz a</p>
+            <p>BeraTop <img src={BeerImage} alt="Logo da BeraTop"></img> </p>
+          </WhoDoes>
+          
+          <InfoDevs 
+            position="left" 
+            dev1={devs[0]} 
+            dev2={devs[1]} 
+          ></InfoDevs>
+          
+          <InfoDevs 
+            position="right" 
+            dev1={devs[2]} 
+            dev2={devs[3]} 
+          ></InfoDevs>
+
+          <InfoDevs 
+            position="left" 
+            dev1={devs[5]} 
+            dev2={devs[4]} 
+          ></InfoDevs>
+
         </div>
       </div>
     </Container>
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,54 +1,5 @@
 import styled from "styled-components";
 
-
-// export const Header = styled.div`
-//     /* border: 1px solid #F3F3F3; */
-//     box-shadow: 0 2px #C4C4C4;
-//     display: flex;
-//     margin: 0 auto;
-//     justify-content: space-around;
-//     width: 90%;
-//     align-items: center;
-//     background-color: #F3F3F3;
-//     height: 80px;
-//     width: 320px;
-//     @media (min-width: 400px) {
-//         width: 400px;
-//     }
-
-//     .logo {
-//         display: flex;
-//         align-items: center;
-
-//         h1 {
-//             color: var(--yellow-1);
-//             font-weight: 700;
-            
-//         }
-
-//         img {
-//             width: 50px;
-//             height: 50px;
-//             filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-//         }
-//     }
-
-//     button {
-//         width: 80px;
-//         height: 27px;
-//         background: var(--grey-0);
-//         color: var(--yellow-1);
-//         padding: 0px;
-//         border-radius: 4px;
-//         border: 1px solid var(--yellow-1);
-//         font-weight: 800;
-//         line-height: 23px;
-//         :hover {
-//             background: var(--grey-1);
-//         }
-//     }
-// `;
-
 export const Container = styled.div`
     width: 320px;
     margin: 0 auto;
@@ -59,7 +10,6 @@ export const Container = styled.div`
     @media (min-width: 400px) {
         width: 400px;
     }
-
 `;
 
 export const TitlePage = styled.div`
@@ -100,7 +50,6 @@ export const TitlePage = styled.div`
 
 export const ParagraphOne = styled.div`
     
-
     display: flex;
     align-items: center;
     height: 160px;
@@ -127,7 +76,6 @@ export const ParagraphOne = styled.div`
 
 export const ParagraphTwo = styled.div`
     
-
     display: flex;
     justify-content: flex-end;
     align-items: center;
@@ -147,9 +95,36 @@ export const ParagraphTwo = styled.div`
     }
 
     img{
-        
         height: 55px;
         width: 46px;
         margin-right: 19px;
+    }
+`;
+
+export const WhoDoes = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 96px;
+    width: 200px;
+    margin-top: 20px;
+    margin-left: 20px;
+
+    p:nth-child(1){
+        font-weight: 600;
+        font-size: 20px;
+        color: #000000;
+    }
+
+    p:nth-child(2){
+        display: flex;
+        align-items: center;
+        font-weight: 700;
+        font-size: 28px;
+        color: #ffc125;
+    }
+
+    p:nth-child(2) > img {
+        height: 60px;
     }
 `;
