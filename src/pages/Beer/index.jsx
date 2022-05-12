b6dd545c6936f9cf5e2b10a0d780872827729cb1
Leonardo Rocha <leonard_rock@outlook.com>
2022-05-12 13:35:03 -0300

    create: a página de cada cerveja  carrega a descrição de acordo com o tamanho da página.

--- src/pages/Beer/index.jsx
+++ src/pages/Beer/index.jsx
@@ -4,20 +4,33 @@ import { useParams } from "react-router-dom";
 import BeerCard from "../../components/BeerCard";
 import { useContext } from "react";
 import { ApiContext } from "../../providers/ApiProvider";
+import { ProductDescription, BeerContainer } from "./styles";
 
 function Beer() {
   const { id } = useParams();
   const { products, setProducts, addReview, editReview, deleteReview } =
     useContext(ApiContext);
+  const deviceWidth = window.innerWidth;
 
   return (
     <>
       <Header bgColor={"#F3F3F3"} />
-      {products.map((product) => {
-        if (product.id == id) {
-          return <BeerCard product={product} key={id} />;
-        }
-      })}
+      <BeerContainer>
+        {products.map((product) => {
+          if (product.id == id) {
+            return (
+              <>
+                <BeerCard product={product} key={id} beerPage={"beerPage"} />
+                {deviceWidth > 700 && (
+                  <ProductDescription>
+                    Resumo: {product.description}
+                  </ProductDescription>
+                )}
+              </>
+            );
+          }
+        })}
+      </BeerContainer>
       <Footer />
     </>
   );
