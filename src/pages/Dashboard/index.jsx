f25cf4450281887574c83a7861579cddebc9a62a
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-05-12 11:37:12 -0300

    Renderização dos produtos na dashboard e alteração para se adequarem ao tamanho do card

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BeersShowcase from "../../components/BeersShowcase";
+
+// import { Container } from './styles';
+
+function Dashboard() {
+  return (
+    <>
+      <BeersShowcase />
+    </>
+  );
+}
+
+export default Dashboard;
